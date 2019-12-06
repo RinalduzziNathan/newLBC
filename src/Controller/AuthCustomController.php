f1ecfb9fd7b08116bbbb45cdb3c5bf91b3e65f6f
Louis GRANGE <46597365+Kraknistic@users.noreply.github.com>
2019-12-06 14:09:12 +0100

    controller

--- src/Controller/AuthCustomController.php
+++ src/Controller/AuthCustomController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Entity\UserImage;
 use App\Entity\UserLogin;
 use App\Form\UserLoginFormType;
+use App\Repository\ImageUserRepository;
+use App\Repository\UserImageRepository;
+use App\Repository\UserLoginRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -89,6 +92,59 @@ class AuthCustomController extends AbstractController
         }
         return $this->render('security/register.html.twig', ['form' => $form->createView()]); // Hop redirigé et on sort du controller
     }
+
+    /**
+     * @Route("/editprofile/{userid}", name="editprofile")
+     */
+    public function editprofile(Request $request, UserPasswordEncoderInterface $encoder,EntityManagerInterface $em, $userid)
+    {
+        $image = $em->getRepository(UserImage::class)->find($userid);
+        $user = new UserLogin();
+        $form = $this->createForm(UserLoginFormType::class,$user);
+        $user = $em->getRepository(UserLogin::class)->find($userid);
+        $form->handleRequest($request); // On récupère le formulaire envoyé dans la requête
+        if ($form->isSubmitted() && $form->isValid()) { // on véfifie si le formulaire est envoyé et si il est valide
+            /** @var UploadedFile $File */
+            $File = $form->get('userImage')->get('filename')->getData();
+
+            $article = $form->getData(); // On récupère l'article associé
+            $user =  $form->getData();
+            $encoded = $encoder->encodePassword($article, $article->getPassword());
+            $article->setPassword($encoded);
+            $article->setCreationdate(New \DateTime());
+            $article->setRoles(['ROLE_USER']);
+
+            if ($File) {
+                $originalFilename = pathinfo($File->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+
+                $safeFilename = preg_replace('/[^A-Za-z0-9]/', "",$originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$File->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $File->move(
+                        $this->getParameter('file_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                $image->setFilename($newFilename);
+                $image->setUser($user);
+                $article->setImageUser($image);
+            }
+
+            $em->persist($image);
+            $em->persist($article); // on le persiste
+            $em->flush(); // on save
+            return $this->redirectToRoute('index');
+        }
+        return $this->render('security/register.html.twig', ['form' => $form->createView()]); // Hop redirigé et on sort du controller
+    }
+
 
     /**
      * @Route("/logout", name="app_logout")
