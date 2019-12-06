da9a98216177d390ec2c6f50c5f11d94e048cb73
Nathan <48697917+RinalduzziNathan@users.noreply.github.com>
2019-12-06 21:05:30 +0100

    Update AuthCustomController.php
    je remets le use qui a disparu dans le dernier commit (?)

--- src/Controller/AuthCustomController.php
+++ src/Controller/AuthCustomController.php
@@ -4,21 +4,22 @@ namespace App\Controller;
 
 use App\Entity\UserImage;
 use App\Entity\UserLogin;
+use App\Form\UserLoginFormType;
 use App\Form\UpdateUserFormType;
 use App\Repository\ImageUserRepository;
 use App\Repository\UserImageRepository;
 use App\Repository\UserLoginRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AuthCustomController extends AbstractController
 {
