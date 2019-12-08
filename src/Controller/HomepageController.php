68745502a85cd6eb1ce2debb4f4f84901a6d8dd3
Louis GRANGE <46597365+Kraknistic@users.noreply.github.com>
2019-12-08 12:49:29 +0100

    css mail

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -49,8 +49,9 @@ class HomepageController extends AbstractController
     {
         $converter = new CssToInlineStyles();
         $css = file_get_contents('../public/css/core-style.css');
-        echo $converter->convert($css);
+        $converter->convert($css);
         $mailer->registerPlugin(new CssInlinerPlugin($converter));
+
         $repository = $em->getRepository(Product::class);
         $products = $repository->findallWithLimit(6);
         $message = (new \Swift_Message('Hello Email'))
