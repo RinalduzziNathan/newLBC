def47aab76b9792dfbee569c23c7ba80afbccbf1
Nathan <48697917+RinalduzziNathan@users.noreply.github.com>
2019-12-09 22:48:59 +0100

    Update APIController.php

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -34,6 +35,25 @@ class APIController extends AbstractController
        
    }
 
+    /**
+     * @Route("v2/product/{category}/{productname}")
+     */
+    public function RestApiTest($category,$productname,EntityManagerInterface $em){
+      
+        $repository = $em->getRepository(Product::class);
+        $product = $repository->findByNameAndCategory($productname, $category);
+   
+    //    $encoder = new JsonEncoder();
+    //    $defaultContext = [
+    //        AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+    //            return $object->getId();
+    //        },
+    //    ];
+    //    $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+    //    $serializer = new Serializer([$normalizer], [$encoder]);
+       return new JsonResponse($product);
+       
+   }
      /**
     * @Route("test")
     */
