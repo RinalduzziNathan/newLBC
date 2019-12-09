d6820ea8a505ef952027a091ee108ef846caf95c
Perrin <perriguigui@gmail.com>
2019-12-09 19:04:17 +0100

    mail

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -53,12 +53,23 @@ class HomepageController extends AbstractController
     /**
      * @Route("/sendmail/{email}", name="sendmail")
      */
-    public function SendMail(\Swift_Mailer $mailer, $email, EntityManagerInterface $em)
+    public function SendMail(\Swift_Mailer $mailer, $email, EntityManagerInterface $em,Request $request)
     {
         $converter = new CssToInlineStyles();
         $css = file_get_contents('../public/css/core-style.css');
         $converter->convert($css);
         $mailer->registerPlugin(new CssInlinerPlugin($converter));
+
+        $formSearch = $this->createForm(SearchProductFormType::class);
+        $formSearch->handleRequest($request); // On récupère le formulaire envoyé dans la requête
+
+        if ($formSearch->isSubmitted() && $formSearch->isValid()) { // on véfifie si le formulaire est envoyé et si il est valide
+            $repository = $em->getRepository(Product::class);
+            $article = $formSearch->getData(); // On récupère l'article associé
+            $name = $article["recherche"];
+            $names = $repository->findByName($name);
+            return $this->render('product/recherche.html.twig', ['formSearch' => $formSearch->createView(),'names'=>$names]); // Hop redirigé et on sort du controller
+        }
 
         $repository = $em->getRepository(Product::class);
         $products = $repository->findallWithLimit(6);
@@ -68,21 +79,23 @@ class HomepageController extends AbstractController
             ->setBody(
                 $this->renderView(
                     'email/email.html.twig', [
-                    "products" => $products
+                    "products" => $products,'formSearch' => $formSearch->createView()
                 ]),'text/html'
             )
             ->addPart(
                 $this->renderView(
                 // templates/emails/registration.txt.twig
                     'email/email.html.twig',
-                    ['products' => $products]
+                    ['products' => $products,'formSearch' => $formSearch->createView()]
                 ),
                 'text/plain'
             )
         ;
         $mailer->send($message);
 
-        return $this->redirectToRoute("index");
+        return $this->redirectToRoute("index", [
+            'formSearch' => $formSearch->createView()
+        ]);
     }
 
     /**
@@ -92,6 +105,7 @@ class HomepageController extends AbstractController
     {
         $repository = $em->getRepository(Product::class);
         $products = $repository->findAll();
+
         return $this->render('email/email.html.twig', [
             "products" => $products
         ]);
